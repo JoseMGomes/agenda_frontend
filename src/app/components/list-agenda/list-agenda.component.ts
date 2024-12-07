ee1411984c0ef57fc850818282b07bb4bcb6364b
José Lucas <gomessze03@gmail.com>
2024-12-07 17:03:39 -0300

    Refactor: auto-format code with Prettier/IDE (Alt+Shift+F)

--- src/app/components/list-agenda/list-agenda.component.ts
+++ src/app/components/list-agenda/list-agenda.component.ts
@@ -5,27 +5,26 @@ import { AgendaService } from '../../services/agenda.service';
 @Component({
   selector: 'app-list-agenda',
   templateUrl: './list-agenda.component.html',
-  styleUrls: ['./list-agenda.component.css']
+  styleUrls: ['./list-agenda.component.css'],
 })
 export class ListAgendaComponent implements OnInit {
   public agendas: Agenda[] = [];
 
-  constructor(private service: AgendaService){}
+  constructor(private service: AgendaService) {}
 
   ngOnInit(): void {
-      this.loadAgenda();
+    this.loadAgenda();
   }
 
-  loadAgenda(){
+  loadAgenda() {
     this.service.getAgenda().subscribe({
-      next: (data)=> (this.agendas = data),
-    })
+      next: (data) => (this.agendas = data),
+    });
   }
 
-  delete(agenda: Agenda){
+  delete(agenda: Agenda) {
     this.service.delete(agenda).subscribe({
-      next: ()=> this.loadAgenda(),
-    })
+      next: () => this.loadAgenda(),
+    });
   }
-
 }

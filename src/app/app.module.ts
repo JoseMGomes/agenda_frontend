bf0be6f05fd70661f00be638a7b2fa2a12087508
José Lucas <gomessze03@gmail.com>
2024-12-07 14:37:40 -0300

    footer, navbar and list-agenda components were created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListAgendaComponent } from './components/list-agenda/list-agenda.component';
+import { FormListComponent } from './components/form-list/form-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    ListAgendaComponent,
+    FormListComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/list-agenda/list-agenda.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-agenda',
+  templateUrl: './list-agenda.component.html',
+  styleUrl: './list-agenda.component.css'
+})
+export class ListAgendaComponent {
+
+}

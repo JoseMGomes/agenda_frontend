b80968a9b0724248bbe0a0014373db2fab45dba0
José Lucas <gomessze03@gmail.com>
2024-12-07 16:44:35 -0300

    all routes were created with routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListAgendaComponent } from './components/list-agenda/list-agenda.component';
+import { FormListComponent } from './components/form-list/form-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'agendas', component: ListAgendaComponent},
+  {path: 'agenda/:id', component: FormListComponent},
+  {path: 'agenda', component: FormListComponent},
+  {path: 'home', component: ListAgendaComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
